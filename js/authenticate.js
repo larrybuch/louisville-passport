f338adab8fdff64b89049a72955f05c23c81dda7
kklai <kkrebeccalai@gmail.com>
2013-11-08 18:34:51 -0500

    Set up foursquare login

---
+++ js/authenticate.js
@@ -0,0 +1,25 @@
+var access_token;
+function authenticateUser(){
+	// let user login
+	if (access_token === undefined) {
+		OAuth.initialize('TGG7iuinPDYvSn25tZmo_OCBgEo');
+		OAuth.popup('foursquare', function(error, result) {
+	  	//handle error with error
+	  	//use result.access_token in your API request
+	  	access_token = result.access_token;
+	  	redirect();
+		});
+	} else {
+		redirect();
+	}
+}
+
+function redirect(){
+	window.location = "success.html" + "?access_token=" + access_token;
+}
+
+$(document).ready(function(){
+	$('.box').click(function(){
+		authenticateUser();
+	})
+});
