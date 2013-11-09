b4555ede5542fef8c64ace8b714101e4327af057
kklai <kkrebeccalai@gmail.com>
2013-11-08 19:31:29 -0500

    Get user information

--- js/loupass.js
+++ js/loupass.js
@@ -1,4 +1,21 @@
+var user_access_token = window.location.search.substring(14);
+var user_info, user_id, first_name, last_name;
+
+function getUser(){
+	$.ajax({
+		type: "GET",
+		url: "https://api.foursquare.com/v2/users/self?oauth_token=" + user_access_token,
+		dataType: "json",
+		success:function(data){
+			user_info = data;
+			user_id = user_info.response.user.id;
+			first_name = user_info.response.user.firstName;
+			last_name = user_info.response.user.lastName;
+			$('.info').html('<h1>Hi ' + first_name + " " + last_name + "!</h1>");
+		}
+	})
+}
+
 $(document).ready(function(){
-	var url = window.location;
-	console.log(document.URL);
+	getUser();
 });
