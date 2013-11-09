2a56420b9a2aa54bb73afa13d830b287f41ff096
kklai <kkrebeccalai@gmail.com>
2013-11-08 19:44:10 -0500

    get user venue history

--- js/loupass.js
+++ js/loupass.js
@@ -1,5 +1,5 @@
 var user_access_token = window.location.search.substring(14);
-var user_info, user_id, first_name, last_name;
+var user_info, user_id, first_name, last_name, venue_history;
 
 function getUser(){
 	$.ajax({
@@ -12,9 +12,21 @@ function getUser(){
 			first_name = user_info.response.user.firstName;
 			last_name = user_info.response.user.lastName;
 			$('.info').html('<h1>Hi ' + first_name + " " + last_name + "!</h1>");
+			getHistory();
 		}
 	})
-}
+};
+
+function getHistory(){
+	$.ajax({
+		type: "GET",
+		url: "https://api.foursquare.com/v2/users/" + user_id + "/venuehistory?oauth_token=" + user_access_token,
+		dataType: "json",
+		success:function(data){
+			venue_history = data;
+		}
+	})
+};
 
 $(document).ready(function(){
 	getUser();
