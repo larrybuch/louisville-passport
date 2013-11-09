c528d8b998b6a2076751c067b1a68324b8b0df26
kklai <kkrebeccalai@gmail.com>
2013-11-08 20:40:43 -0500

    check if a user have been to our list of places

--- js/loupass.js
+++ js/loupass.js
@@ -1,10 +1,18 @@
 var user_access_token = window.location.search.substring(14);
 var user_info, user_id, first_name, last_name, venue_history;
-
-var venues = ['Galt House Hotel'];
-var matches = {
-	'Galt House Hotel': 0
-}
+var matches = [
+	{name: 'Galt House Hotel', beenHere: 0},
+	{name: "Muhammad Ali Center", beenHere: 0},
+	{name: "Kentucky Museum of Art and Craft (KMAC)", beenHere: 0},
+	{name: "Churchill Downs", beenHere: 0}, 
+	{name: "Joe Huber's Family Farm & Rest.", beenHere: 0},
+	{name: "Louisville Slugger Museum & Factory", beenHere: 0}, 
+	{name: "Belle of Louisville", beenHere: 0}, 
+	{name: "Cave Hill National Cemetery", beenHere: 0}, 
+	{name: "Kentucky Derby Museum", beenHere: 0},
+	{name: "Kentucky Science Center", beenHere: 0}, 
+	{name: "Louisville Mega Cavern", beenHere: 0}
+]
 
 function getUser(){
 	$.ajax({
@@ -36,9 +44,9 @@ function getHistory(){
 
 function checkPlace(){
 	for (var i=0; i<venue_history.length; i++) {
-		for (var j=0; j<venues.length; j++){
-			if (venue_history[i].venue.name === venues[j]){
-				matches.venues[j] = venue_history[i].beenHere;
+		for (var j=0; j<matches.length; j++){
+			if (venue_history[i].venue.name === matches[j].name){
+				matches[j].beenHere = true;
 			}
 		}
 	}
